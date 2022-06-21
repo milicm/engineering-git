1b38e89b2d9be43b8f9e0a2674898305a7b8957c
Milos Milic <mmilic@gmail.com>
2022-06-21 13:45:22 +0200

    Added implementation of sum() funcition

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var x = 0;
 var y = 0;
+
+var array = [1, 2, 3, 4];
+
+function sum(array) {
+    var s = 0;
+    for (i = 0; i < array.length; i++) {
+        s += array[i];
+    }
+    return s;
+}
+
